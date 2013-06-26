6258abf5eefe66382bc5eaa074321cfdb30cda6a
channyeinkyaw <channyeinkyaw@gmail.com>
2013-06-26 15:08:50 +0900

    update

---
+++ protected/views/site/index.php
@@ -0,0 +1,36 @@
+<?php
+/* @var $this SiteController */
+
+$this->pageTitle=Yii::app()->name;
+?>
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/table.css" />
+</head>
+  <body>
+<h1>Welcome to <i><?php echo CHtml::encode(Yii::app()->name); ?></i></h1>
+
+<div id="login">
+  Hello Framework!<br>
+  Hello Test
+</div>
+<tabel class="myTable">
+  <tr><th>Hello</th><th>Hello1</th><th>Hello2</th></tr>
+  <tr><td>hi</td><td>hi1</td><td>hi2</td></tr>
+</tabel>
+<p>Congratulations! You have successfully created your Yii application.</p>
+
+<p>You may change the content of this page by modifying the following two files:</p>
+<ul>
+	<li>View file: <code><?php echo __FILE__; ?></code></li>
+	<li>Layout file: <code><?php echo $this->getLayoutFile('main'); ?></code></li>
+</ul>
+
+<p>For more details on how to further develop this application, please read
+the <a href="http://www.yiiframework.com/doc/">documentation</a>.
+Feel free to ask in the <a href="http://www.yiiframework.com/forum/">forum</a>,
+should you have any questions.</p>
+</body>
+</html>
